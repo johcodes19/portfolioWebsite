9fe081b863c787d2e56750253e525bec1a35e6bf
johCodes <johcodes19@gmail.com>
2024-01-29 13:17:22 +0300

    created the backend functionality of the website using sanity

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,10 @@
+import testimonials from './testimonials'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+import works from './works'
+
+export const schemaTypes = [ testimonials, brands, abouts, experiences, skills, workExperience, contact, works]
